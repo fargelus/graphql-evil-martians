5b4540912cc19b65691fceef5b52620d233ae9a8
Dmitriy Nemykin <ddraudred@gmail.com>
2020-06-06 10:48:31 +0300

    Mutations for add/update items

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :sign_in, mutation: Mutations::SignInMutation
+    field :add_item, mutation: Mutations::AddItemMutation
+    field :update_item, mutation: Mutations::UpdateItemMutation
   end
 end
