89d65f25d27807f59ff26e5e0350b1f1e22534b7
Dmitriy Nemykin <ddraudred@gmail.com>
2020-06-16 08:49:24 +0300

    Some DRY for code

--- app/graphql/mutations/add_item_mutation.rb
+++ app/graphql/mutations/add_item_mutation.rb
@@ -7,13 +7,10 @@ module Mutations
     argument :image_url, String, required: false
 
     field :item, Types::ItemType, null: true
-    field :errors, [String], null: false
+    field :errors, Types::ValidationErrors, null: true
 
     def resolve(title:, description: nil, image_url: nil)
-      if context[:current_user].nil?
-        raise GraphQL::ExecutionError,
-              'You need to authenticate to perform this action'
-      end
+      check_authentication!
 
       item = Item.new(
         title: title,
@@ -25,7 +22,7 @@ module Mutations
       if item.save
         { item: item }
       else
-        { errors: item.errors.full_messages }
+        { errors: item.errors }
       end
     end
   end
--- app/graphql/mutations/update_item_mutation.rb
+++ app/graphql/mutations/update_item_mutation.rb
@@ -8,13 +8,10 @@ module Mutations
     argument :image_url, String, required: false
 
     field :item, Types::ItemType, null: true
-    field :errors, [String], null: false
+    field :errors, Types::ValidationErrors, null: true
 
     def resolve(id:, title:, description: nil, image_url: nil)
-      if context[:current_user].nil?
-        raise GraphQL::ExecutionError,
-              'You need to authenticate to perform this action'
-      end
+      check_authentication!
 
       item = Item.find(id)
       if item.update(
@@ -24,7 +21,7 @@ module Mutations
       )
         { item: item }
       else
-        { errors: item.errors.full_messages }
+        { errors: item.errors }
       end
     end
   end
